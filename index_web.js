2b973943656d2fc418b9f0f5ac06c0c9de511179
ollyollyQq <2016olly9999999999@gmail.com>
2022-12-17 22:11:26 +0800

    new one

--- index_web.js
+++ index_web.js
@@ -1,7 +1,9 @@
 let http = require("http");
 let fs = require("fs");
 http.createServer(function(req,res){
-    let html = fs.readFileSync(`${__dirname}/index.html`);
+    let html = fs.readFileSync(`${__dirname}/index.html`,"utf8");
+    let date = new Date();
+    html = html.replace("{DateMsg}",date);
     res.writeHead(200, {"contentType":"text/html"});
     res.end(html);
   }).listen(5678,"127.0.0.1");
--- webserver.js
+++ webserver.js
@@ -1,7 +1,7 @@
 let http = require("http");
 let fs = require("fs");
-http.createServer(function(req,res){
-  let html = fs.readFileSync(`${__dirname}/index.html`,"utf8");
-  res.writeHead(200,{"Content-Type":"text/plain"});
-  res.end("Hello~\n");
+http.createServer(function(req,res){ 
+  let html = fs.readFileSync(`${__dirname}/index.html`); 
+  res.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"});
+  res.end("Hello~\nI'm olly come from taiwan\nThis is my first websiteserver\n鏈鋸人好看-------我愛瑪其瑪");
 }).listen(1234,"127.0.0.1");
